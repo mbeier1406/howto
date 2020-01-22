730f3b55a2818a1def6f60d449e46a6eeefaef33
Martin Beier <Martin.Beier@gmx.de>
2020-01-22 21:32:10 +0100

    Zwischenstand Veröffentlichung Maven Central

--- jse/src/main/java/de/gmxhome/golkonda/howto/easyrandom/Datenspeicher.java
+++ jse/src/main/java/de/gmxhome/golkonda/howto/easyrandom/Datenspeicher.java
@@ -3,10 +3,10 @@ package de.gmxhome.golkonda.howto.easyrandom;
 import java.util.List;
 
 /**
- * Dieses Objekt soll mittels <a href="https://github.com/j-easy/easy-random">Easy Random</a> mit verschiedenen
+ * <p>Dieses Objekt soll mittels <a href="https://github.com/j-easy/easy-random">Easy Random</a> mit verschiedenen
  * Daten gefüllt werden. Dabei kommen sowohl einfache Datentypen als auch komplexe Objekte zum Einsatz.
  * Dabei sollen die Werte nicht völlig zufällig generiert werden, sondern sich in definierten Bereichen befinden.
- * Das Objekt hat ansonsten keine weitere Logik. Der Test befindet sich in {@code DatenspeicherTest}.<p/>
+ * Das Objekt hat ansonsten keine weitere Logik. Der Test befindet sich in {@code DatenspeicherTest}.</p>
  * <u>Hinweis</u>: zum Sertzen der Felder werden keine <em>Setter</em> benötigt, obwohl diese als <code>private</code>
  * definiert sind, da die API mit Reflections arbeitet!
  * @author mbeier
