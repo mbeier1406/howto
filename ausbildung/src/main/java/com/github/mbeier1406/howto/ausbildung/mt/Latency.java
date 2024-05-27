ab3737609de552bb437d299c72ee016c9f4b2355
Martin Beier <Martin.Beier@gmx.de>
2024-05-27 22:03:09 +0200

    [Ausbildung] Multithreading: Performancemessung (Latenz)

--- ausbildung/src/main/java/com/github/mbeier1406/howto/ausbildung/mt/Latency.java
+++ ausbildung/src/main/java/com/github/mbeier1406/howto/ausbildung/mt/Latency.java
@@ -13,11 +13,14 @@ public class Latency {
 
 	public static final Logger LOGGER = LogManager.getLogger(Latency.class);
 
+	/**
+	 * Definiert die Maske und Shiftwerte für ARGB-Pixel.
+	 */
 	public static enum ARGB {
-		ALPHA(0xff000000, 24),
-		RED(0x00ff0000, 16),
-		GREEN(0x0000ff00, 8),
-		BLUE(0x000000ff, 0);
+		ALPHA(0xff000000, 24), // Transparenz
+		RED(0x00ff0000, 16),   // Rotwert
+		GREEN(0x0000ff00, 8),  // Grünwert
+		BLUE(0x000000ff, 0);   // Blauwert
 		private int mask, shift;
 		private ARGB(int mask, int shift) {
 			this.mask = mask;
@@ -39,6 +42,17 @@ public class Latency {
 	 */
 	public static int getArgb(ARGB argb, int value) {
 		return ( value & argb.getMask() ) >> argb.getShift();
+	}
+
+	/**
+	 * Ereugt einen ARGB-Pixel aus den drei Farbwerten. Der Alpha-Wert wird auf <b>0xff</b> gesetzt (keine Transparenz).
+	 * @param red der Rotwert
+	 * @param green der Grünwert
+	 * @param blue der Blauwert
+	 * @return der resultierende ARGB-Wert
+	 */
+	public static int createRgbFromColors(int red, int green, int blue) {
+		return ((((0 | blue  << ARGB.BLUE.getShift()) | green << ARGB.GREEN.getShift()) | red << ARGB.RED.getShift()) | 0xff << ARGB.ALPHA.getShift());
 	}
 
 }
--- ausbildung/src/test/java/com/github/mbeier1406/howto/ausbildung/mt/LatencyTest.java
+++ ausbildung/src/test/java/com/github/mbeier1406/howto/ausbildung/mt/LatencyTest.java
@@ -6,10 +6,10 @@ import static com.github.mbeier1406.howto.ausbildung.mt.Latency.ARGB.GREEN;
 import static com.github.mbeier1406.howto.ausbildung.mt.Latency.ARGB.BLUE;
 import static com.github.mbeier1406.howto.ausbildung.mt.Latency.ARGB.RED;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -23,25 +23,32 @@ public class LatencyTest {
 	/** Stellt sicher, dass der Alpha-Wert eines ARGB-Pixels korrekt ermittelt wird */
 	@Test
 	public void testeAlphaWert() {
-		assertThat(Latency.getArgb(ALPHA, (int) 0x12345678), Matchers.equalTo(18));
+		assertThat(Latency.getArgb(ALPHA, (int) 0x12345678), equalTo(18));
 	}
 
 	/** Stellt sicher, dass der Rot-Wert eines ARGB-Pixels korrekt ermittelt wird */
 	@Test
 	public void testeRotWert() {
-		assertThat(Latency.getArgb(RED, (int) 0x12345678), Matchers.equalTo(52));
+		assertThat(Latency.getArgb(RED, (int) 0x12345678), equalTo(52));
 	}
 
 	/** Stellt sicher, dass der Grün-Wert eines ARGB-Pixels korrekt ermittelt wird */
 	@Test
 	public void testeGruenWert() {
-		assertThat(getArgb(GREEN, (int) 0x12345678), Matchers.equalTo(86));
+		assertThat(getArgb(GREEN, (int) 0x12345678), equalTo(86));
 	}
 
 	/** Stellt sicher, dass der Blau-Wert eines ARGB-Pixels korrekt ermittelt wird */
 	@Test
 	public void testeBlauWert() {
-		assertThat(getArgb(BLUE, (int) 0x12345678), Matchers.equalTo(120));
+		assertThat(getArgb(BLUE, (int) 0x12345678), equalTo(120));
+	}
+
+	@Test
+	public void testeCreateRgbFromColors() {
+		int rgb = Latency.createRgbFromColors(18, 52, 86);
+		LOGGER.info("rgb={}", rgb);
+		assertThat(rgb, equalTo(-15584170));
 	}
 
 }
