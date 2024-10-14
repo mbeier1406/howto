ffb1bf7f289e4071a61ed163af5558c1c5bb4f60
Martin Beier <Martin.Beier@gmx.de>
2024-10-14 10:55:34 +0200

    Auwertung mathematischer Formeln (Rechnerfunktion)

---
+++ ausbildung/src/main/java/com/github/mbeier1406/howto/ausbildung/rechner/token/GanzzahlToken.java
@@ -0,0 +1,65 @@
+package com.github.mbeier1406.howto.ausbildung.rechner.token;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.github.mbeier1406.howto.ausbildung.rechner.TokenInterface;
+import com.google.common.primitives.Chars;
+
+/**
+ * Das Token für eine ganze Zahl.
+ */
+@Token
+public class GanzzahlToken implements TokenInterface {
+
+	private static final char[] SYMBOL_LIST = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+	/** {@value #SYMBOL_LIST} als Liste zum schnellen Suchen */
+	private static List<Character> SYMBOLS;
+
+	static {
+		SYMBOLS = Chars.asList(SYMBOL_LIST);
+	}
+
+	/** Wert des Tokens */
+	private final Integer value;
+
+	/** {@inheritDoc} */
+	@Override
+	public Value read(String text) {
+		if ( requireNonNull(text, "text").length() < 1 || !SYMBOLS.contains(text.charAt(0)) )
+			throw new IllegalArgumentException("Ziffer erwartet: '"+text+"'");
+		final StringBuilder sb = new StringBuilder(String.valueOf(text.charAt(0)));
+		while ( sb.length() < text.length() && text.charAt(sb.length()) >= '0' && text.charAt(sb.length()) <= '9' )
+			sb.append(text.charAt(sb.length()));
+		return new Value(new GanzzahlToken(Integer.parseInt(sb.toString())), sb.length()); // Ganzzahl mit x Ziffern
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public char[] getSymbol() {
+		return SYMBOL_LIST;
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public Optional<Object> getValue() {
+		return Optional.of(requireNonNull(value, "Kein Token eingelesen!"));
+	}
+
+	/** Konstruktor, um ein Ganzzahl-Token einlesen zu können */
+	public GanzzahlToken() { this.value = null; }
+
+	/** Konstruktor zur Erzeugung eines Tokens */
+	private GanzzahlToken(int value) {
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return "GanzzahlToken [value=" + value + "]";
+	}
+
+}
---
+++ ausbildung/src/test/java/com/github/mbeier1406/howto/ausbildung/rechner/token/GanzzahlTokenTest.java
@@ -0,0 +1,77 @@
+package com.github.mbeier1406.howto.ausbildung.rechner.token;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.stream.Stream;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import com.github.mbeier1406.howto.ausbildung.rechner.TokenInterface.Value;
+
+/**
+ * Test für die Klasse {@linkplain GanzzahlToken}.
+ */
+public class GanzzahlTokenTest {
+
+	public static final Logger LOGGER = LogManager.getLogger(GanzzahlTokenTest.class);
+
+	/** Das zu testende Objekt */
+	public GanzzahlToken ganzzahlToken = new GanzzahlToken();
+
+
+	/** Testet verschiedene Ganzzahlen */
+	@ParameterizedTest
+	@MethodSource("getTestdaten")
+	public void testeEinlesen(String input, int ergebnis) {
+		Value value = ganzzahlToken.read(input);
+		LOGGER.info("input='{}'; value={}", input, value);
+		assertThat(value.token().getClass(), equalTo(GanzzahlToken.class));
+		assertThat(value.token().getValue().get(), equalTo(ergebnis));
+		assertThat(value.length(), equalTo(String.valueOf(value.token().getValue().get()).length()));
+	}
+
+	/** Liefert die Testdaten für den parametrisierten Test {@linkplain #testeEinlesen(String, int)} */
+	public static Stream<Arguments> getTestdaten() {
+		return Stream.of(
+				Arguments.of("12 3", 12),
+				Arguments.of("123", 123),
+				Arguments.of("1 ", 1),
+				Arguments.of("0", 0));
+	}
+
+
+	/** Text muss mit einer Ziffer beginnen */
+	@Test
+	public void testeFalschesSymbol() {
+		assertThrows(IllegalArgumentException.class, () -> new GanzzahlToken().read("abc") );
+	}
+
+	/** Text darf nicht leer sein */
+	@Test
+	public void testeLeererTextl() {
+		assertThrows(IllegalArgumentException.class, () -> new GanzzahlToken().read("") );
+	}
+
+	/** Text darf nicht <b>null</b> sein */
+	@Test
+	public void testeNullTextl() {
+		final NullPointerException ex = assertThrows(NullPointerException.class, () -> new GanzzahlToken().read(null) );
+		assertThat(ex.getMessage(), containsString("text"));
+	}
+
+	/** Das Token zum Einlesen von {@linkplain GanzzahlToken} enthält selber keine Werte */
+	@Test
+	public void testeReaderToken() {
+		final NullPointerException ex = assertThrows(NullPointerException.class, () -> ganzzahlToken.getValue() );
+		assertThat(ex.getMessage(), equalTo("Kein Token eingelesen!"));
+	}
+
+}
